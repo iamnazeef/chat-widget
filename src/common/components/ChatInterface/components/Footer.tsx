097aafec109fb33dad5b7f3a1b704c4ee3d1d548
iamnazeef <muhammednazeefedu@gmail.com>
2025-01-11 21:08:52 +0530

    convert css classnames to camelcase

--- src/common/components/ChatInterface/components/Footer.tsx
+++ src/common/components/ChatInterface/components/Footer.tsx
@@ -1,11 +1,11 @@
-import OrcaLogo from "../../../icons/ic-orca-logo.svg";
+import orcaLogo from "../../../icons/ic-orca-logo.svg";
 import styles from "./styles/Footer.module.css";
 
 const Footer = () => {
     return (
-        <div className={styles.Footer}>
-            <a className={styles.HyperLink} href="https://getorca.ai" target="_blank" rel="noopener">
-                <img src={OrcaLogo} width={20} height={18} />
+        <div className={styles.footer}>
+            <a className={styles.hyperLink} href="https://getorca.ai" target="_blank" rel="noopener">
+                <img src={orcaLogo} width={20} height={18} />
                 <p>Powered by ORCA AI</p>
             </a>
         </div>
--- src/common/components/ChatInterface/components/Toolbar.tsx
+++ src/common/components/ChatInterface/components/Toolbar.tsx
@@ -1,29 +1,29 @@
 import styles from "./styles/Toolbar.module.css";
-import NewChatIcon from "../../../icons/ic-new-chat.svg";
-import ChatHistoryIcon from "../../../icons/ic-chat-history.svg";
+import newChatIcon from "../../../icons/ic-new-chat.svg";
+import chatHistoryIcon from "../../../icons/ic-chat-history.svg";
 
 const Toolbar = () => {
     return (
-        <div className={styles.Toolbar}>
-            <div className={styles.Logo}>
+        <div className={styles.toolbar}>
+            <div className={styles.logo}>
                 <img 
                     height="22px" 
                     width="22px" 
                     className="png" 
                     src="https://app.getorca.ai/docgen/assets/docGenLogo-960f4560.png" 
                 />
-                <p className={styles.Name}>Orca</p>
+                <p className={styles.name}>Orca</p>
             </div>
-            <div className={styles.Actions}>
-                <button className={styles.NewChat}>
-                    <img src={NewChatIcon} />
+            <div className={styles.actions}>
+                <button className={styles.newChat}>
+                    <img src={newChatIcon} />
                 </button>
-                <button className={styles.ChatHistory}>
-                    <img src={ChatHistoryIcon} />
+                <button className={styles.chatHistory}>
+                    <img src={chatHistoryIcon} />
                 </button>
             </div>
         </div>
     )
 }
 
-export default Toolbar; 
+export default Toolbar;
