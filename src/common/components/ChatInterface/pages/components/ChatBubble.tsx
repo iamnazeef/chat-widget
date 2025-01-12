5879a641736b5bd4e9ba57e3041fdd0cd2e7ead1
iamnazeef <muhammednazeefedu@gmail.com>
2025-01-12 11:11:13 +0530

    Refactor chat bubble

--- src/common/components/ChatInterface/pages/components/ChatBubble.tsx
+++ src/common/components/ChatInterface/pages/components/ChatBubble.tsx
@@ -3,14 +3,37 @@ import styles from "../styles/ChatBubble.module.css";
 
 interface IChatBubble {
     children: React.ReactNode;
-    align: "left" | "right"; 
+    align: "left" | "right";
+    leftBubbleIcon?: React.ReactNode;
+    rightBubbleIcon?: React.ReactNode;
+    backgroundColor?: string;
 }
 
-const ChatBubble: FC<IChatBubble> = ({ children, align }) => {
+const ChatBubble: FC<IChatBubble> = ({ 
+    children, align, backgroundColor, leftBubbleIcon, rightBubbleIcon }) => {
 
     return (
-        <div id="chat-interface-bubble" className={`${styles.chatBubble} ${styles[align]}`}>
-            {children}
+        <div 
+            id="chat-interface-bubble-container" 
+            className={styles.chatBubbleContainer}
+        >
+            {align === "left" && 
+            <div 
+                id="avatar" 
+                className={styles.leftBubbleIcon}
+            >
+                {leftBubbleIcon}
+            </div>}
+            <div
+                id="chat-interface-bubble" 
+                style={{ 
+                    backgroundColor,
+                }} 
+                className={`${styles.chatBubble} ${styles[align]}`}
+             >
+                {children}
+            </div>
+            {align === "right" && <div id="avatar" className={styles.rightBubbleIcon}>{rightBubbleIcon}</div>}
         </div>
     )
 }
